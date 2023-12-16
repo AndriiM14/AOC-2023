8c50bfe90314b9f057200a16bff3b45c7d601741
Andrii M <andrii.marusyk.knm.2020@lpnu.ua>
2023-12-16 20:00:38 +0200

    solved part two of day 3 problem

--- day3/solution.dart
+++ day3/solution.dart
@@ -1,20 +1,35 @@
-import 'dart:async';
 import 'dart:io';
 
 bool isSymbol(String char) {
   return char != '.' && int.tryParse(char) == null;
 }
 
-bool containsSymbol(String str) {
-  for (final char in str.split('')) {
-    if (isSymbol(char)) return true;
+bool isGear(String char) {
+  return char == '*';
+}
+
+(bool, bool, (int, int)) containsSymbol(String str, int row, int left) {
+  var contains = false;
+  var containsGear = false;
+  var gearCoordinates = (-1, -1);
+
+  for (final (col, char) in str.split('').indexed) {
+    if (isSymbol(char)) {
+      contains = true;
+
+      if (isGear(char)) {
+        containsGear = true;
+        gearCoordinates = (row, col + left);
+      }
+    }
   }
 
-  return false;
+  return (contains, containsGear, gearCoordinates);
 }
 
 void main(List<String> args) {
   final input = File(args[0]).readAsLinesSync();
+  var gears = Map<(int, int), List<int>>();
 
   var res = 0;
 
@@ -34,31 +49,84 @@ void main(List<String> args) {
       }
       if (left != -1 && right != -1) {
         final num = int.parse(line.substring(left, right + 1));
-        print('Found number: $num');
+        //print('Found number: $num');
         var isPart = false;
 
-        if (left != 0 && isSymbol(line[left - 1])) isPart = true;
-        if (right != line.length - 1 && isSymbol(line[right + 1]))
-          isPart = true;
+        if (left != 0) {
+          if (isSymbol(line[left - 1])) {
+            isPart = true;
+
+            if (isGear(line[left - 1])) {
+              final gear = (lineIdx, left - 1);
+              if (gears.containsKey(gear))
+                gears[gear]?.add(num);
+              else
+                gears[gear] = [num];
+            }
+          }
+        }
+
+        if (right != line.length - 1) {
+          if (isSymbol(line[right + 1])) {
+            isPart = true;
+
+            if (isGear(line[right + 1])) {
+              final gear = (lineIdx, right + 1);
+              if (gears.containsKey(gear))
+                gears[gear]?.add(num);
+              else
+                gears[gear] = [num];
+            }
+          }
+        }
 
         if (left != 0) left -= 1;
         if (right != line.length - 1) right += 1;
 
-        if (lineIdx != 0 &&
-            containsSymbol(input[lineIdx - 1].substring(left, right + 1)))
-          isPart = true;
-        if (lineIdx != input.length - 1 &&
-            containsSymbol(input[lineIdx + 1].substring(left, right + 1)))
-          isPart = true;
+        if (lineIdx != 0) {
+          var (contains, containsGear, gearCoordinates) = containsSymbol(
+              input[lineIdx - 1].substring(left, right + 1), lineIdx - 1, left);
 
-        print('Number: $num; Is a part number: $isPart');
-        if (isPart) res += num;
+          if (contains) {
+            isPart = true;
+            if (containsGear) {
+              if (gears.containsKey(gearCoordinates))
+                gears[gearCoordinates]?.add(num);
+              else
+                gears[gearCoordinates] = [num];
+            }
+          }
+        }
+
+        if (lineIdx != input.length - 1) {
+          var (contains, containsGear, gearCoordinates) = containsSymbol(
+              input[lineIdx + 1].substring(left, right + 1), lineIdx + 1, left);
+
+          if (contains) {
+            isPart = true;
+            if (containsGear) {
+              if (gears.containsKey(gearCoordinates))
+                gears[gearCoordinates]?.add(num);
+              else
+                gears[gearCoordinates] = [num];
+            }
+          }
+        }
+
+        //print('Number: $num; Is a part number: $isPart');
 
         left = -1;
         right = -1;
       }
     }
   }
+
+  gears.forEach((gear, nums) {
+    if (nums.length == 2) {
+      print('Found a gear: $gear');
+      res += nums[0] * nums[1];
+    }
+  });
 
   print('\nResult: $res');
 }
